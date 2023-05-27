e2d63966e72c363497f8367a69423bae18f36525
D-Elbadawi <130947785+D-Elbadawi@users.noreply.github.com>
2023-05-26 20:19:56 -0400

    Update Program.cs

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -1,10 +1,29 @@
-﻿namespace Assignment
+﻿using System;
+
+namespace Assignment
 {
-    static class Program
+    class Program
     {
-        static void Main()
+        static void Main(string[] args)
         {
-            System.Console.WriteLine("Hello World");
+            // Create a treasure chest
+            TreasureChest chest = new TreasureChest();
+
+            // Test default constructor and GetState method
+            Console.WriteLine($"Initial State: {chest.GetState()}");
+
+            // Test Manipulate method with different actions
+            Console.WriteLine($"New State: {chest.Manipulate(TreasureChest.Action.Open)}");
+            Console.WriteLine($"New State: {chest.Manipulate(TreasureChest.Action.Lock)}");
+            Console.WriteLine($"New State: {chest.Manipulate(TreasureChest.Action.Unlock)}");
+            Console.WriteLine($"New State: {chest.Manipulate(TreasureChest.Action.Close)}");
+            Console.WriteLine($"New State: {chest.Manipulate(TreasureChest.Action.Open)}");
+
+            // Test parameterized constructor and ToString method
+            TreasureChest customChest = new TreasureChest(TreasureChest.Material.RichMahogany,
+                                                         TreasureChest.LockType.Intermediate,
+                                                         TreasureChest.LootQuality.Purple);
+            Console.WriteLine(customChest.ToString());
         }
     }
 }
