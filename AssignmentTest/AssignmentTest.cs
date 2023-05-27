6cc4bbec5bdd8f4c7b3e2af8f7415951ecbb16ec
Dabebe100 <130933203+Dabebe100@users.noreply.github.com>
2023-05-27 04:25:48 +0100

    Update AssignmentTest.cs

--- AssignmentTest/AssignmentTest.cs
+++ AssignmentTest/AssignmentTest.cs
@@ -4,9 +4,7 @@ namespace AssignmentTest
 {
     [TestClass]
     public class AssignmentTests
-    {
-        [TestMethod]
-        public void OpenLockedTest()
+    {public void OpenLockedTest()
         {
             // Chest starts in the locked state
             TreasureChest chest = new TreasureChest(TreasureChest.State.Locked);
@@ -14,19 +12,13 @@ namespace AssignmentTest
             // Verify chest is still locked
             chest.Open();
             Assert.AreEqual(chest.GetState(), TreasureChest.State.Locked);
-        }
-
-        [TestMethod]
-        public void OpenClosedTest()
+        }public void OpenClosedTest()
         {
             // Create a new chest that is in the closed state
             TreasureChest chest = new TreasureChest(TreasureChest.State.Closed);
             chest.Open();
             Assert.AreEqual(chest.GetState(), TreasureChest.State.Open);
-        }
-
-        [TestMethod]
-        public void OpenOpenTest()
+        }        public void OpenOpenTest()
         {
             // Create a new chest that is in the open state
             TreasureChest chest = new TreasureChest(TreasureChest.State.Open);
