fa61c06b97e66e78dd5c91ce7ee418ea37d16a3d
Dabebe100 <130933203+Dabebe100@users.noreply.github.com>
2023-05-27 04:29:25 +0100

    Update Program.cs

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -1,10 +1,13 @@
-﻿namespace Assignment
+namespace Assignment
 {
     static class Program
     {
         static void Main()
         {
             System.Console.WriteLine("Hello World");
+            TreasureChest chest = new TreasureChest(TreasureChest.State.Closed);
+            chest.Manipulate(TreasureChest.Action.Open);
+            System.Console.WriteLine(chest);
         }
     }
 }
