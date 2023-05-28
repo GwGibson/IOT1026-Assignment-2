3b6f77a3e546e055dfda41145c624e3f263fd2e1
A00241382@mycambrian.ca <123224843+rebeccaYMCG@users.noreply.github.com>
2023-05-28 10:30:39 -0400

    Revised To Include Additional Test Cases

--- AssignmentTest/AssignmentTest.cs
+++ AssignmentTest/AssignmentTest.cs
@@ -6,9 +6,30 @@ namespace AssignmentTest
     public class AssignmentTests
     {
         [TestMethod]
-        public void DummyTest()
+        public void OpenLockedTest()
         {
-            Assert.AreNotSame(1, 2);
+            // chest starts in the locked state
+            TreasureChest chest = new TreasureChest(TreasureChest.State.Locked);
+            // try to open the chest
+            // verify chest is still locked
+            chest.Open();
+            Assert.AreEqual(chest.GetState(), TreasureChest.State.Locked);
+        }
+        [TestMethod]
+        public void OpenClosedTest()
+        {
+            // create a new chest that is in the closed state
+            TreasureChest chest = new TreasureChest(TreasureChest.State.Closed);
+            chest.Open();
+            Assert.AreEqual(chest.GetState(), TreasureChest.State.Open);
+        }
+        [TestMethod]
+        public void OpenOpenTest()
+        {
+            // create a new chest that is in the open state
+            TreasureChest chest = new TreasureChest(TreasureChest.State.Open);
+            chest.Open();
+            Assert.AreEqual(chest.GetState(), TreasureChest.State.Open);
         }
     }
 }
